b8a63b42caf4d5b8b52e005b1f1db8665242f0a9
Saran Kumar Seetini <119718662+sarankumar0101@users.noreply.github.com>
2025-01-30 11:34:12 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (60.99%) - LeetSync

---
+++ 782-jewels-and-stones/jewels-and-stones.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def numJewelsInStones(self, jewels, stones):
+        count = 0
+        for char in stones:
+            if char in jewels:
+                count +=1
+        return count
+        
+        
