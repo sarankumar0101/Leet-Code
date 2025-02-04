8d37b519994e6fb492634afcb81998f2bb793105
Saran Kumar Seetini <119718662+sarankumar0101@users.noreply.github.com>
2025-02-04 10:28:49 +0530

    Time: 0 ms (100.00%) | Memory: 12.6 MB (5.21%) - LeetSync

---
+++ 1293-three-consecutive-odds/three-consecutive-odds.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def threeConsecutiveOdds(self, arr):
+        for i in range(len(arr) - 2):
+            if (arr[i]%2 !=0 and arr[(i+1)]%2 !=0 and arr[(i+2)]%2 !=0):
+                return(True)
+                break
+        else:
+            return(False)
+        
